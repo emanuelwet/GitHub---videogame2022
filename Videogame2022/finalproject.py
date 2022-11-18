edee081ce7058936d99e406535ad8e58d180a0e3
emanuelwet <115042778+emanuelwet@users.noreply.github.com>
2022-11-17 22:54:21 -0800

    Update finalproject.py

--- Videogame2022/finalproject.py
+++ Videogame2022/finalproject.py
@@ -1,2 +1,51 @@
-#pong game 
-#need two sides that can bounce a ball either vertically or horizontally
+Project Title
+Simple overview of use/purpose.
+   #pong game 
+
+Description
+An in-depth paragraph about your project and overview of use.
+  #need two sides that can bounce a ball either vertically or horizontally. The goal of the game is to score as many points as possible by forcing the ball to go past the opponent's platform
+
+Getting Started
+Dependencies
+Describe any prerequisites, libraries, OS version, etc., needed before installing program.
+ex. Python 3.x, Pygame, etc.
+   #I do not know yet, since I have not started working on the code
+
+
+Installing
+How/where to download your program
+  #Use the github link 
+
+Any modifications needed to be made to files/folders
+Executing program
+How to run the program
+  # after pressing play use the "w" and "s" buttons to move the platform vertically/ horizontally (have not decided which direction)
+  
+Step-by-step bullets
+code blocks for commands
+Help
+Any advise for common problems or issues.
+
+command to run if program contains helper info
+Authors
+Contributors names and contact info
+
+ex. Chris Cozort
+ex. ccozort@bcp.org
+
+Version History
+0.2
+Various bug fixes and optimizations
+See commit change or See release history
+0.1
+Initial Release
+License
+This project is licensed under the [NAME HERE] License - see the LICENSE.md file for details
+
+Acknowledgments
+Inspiration, code snippets, etc.
+
+w3Schools
+PyGame
+Automate The Boring Stuff
