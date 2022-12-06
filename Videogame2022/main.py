000c87ea7c21eb9b85c744e7ced9af415892bb4a
emanuelwet <Emanuel.wetschnig@gmail.com>
2022-12-06 11:02:48 -0800

    UPDATE

--- Videogame2022/main.py
+++ Videogame2022/main.py
@@ -1,41 +1,50 @@
-#pong game 
-#need two sides that can bounce a ball either vertically or horizontally
+# content from kids can code: http://kidscancode.org/blog/
 
-#import libraries and modules
-#from platform import platform
-
+# import libraries and modules
 import pygame as pg
 from pygame.sprite import Sprite
 import random
-from random import randint
+
+
+# game settings
+WIDTH = 360
+HEIGHT = 450
+FPS = 30
+
+
+vec = pg.math.Vector2
 
 # game settings 
-WIDTH = 850
-HEIGHT = 800
+WIDTH = 360
+HEIGHT = 480
 FPS = 30
 
 # define colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
 GRAY = (128, 128, 128, 255)
 
-
-# initiate pygame and window is created
+# init pygame and create a window
 pg.init()
 pg.mixer.init()
 screen = pg.display.set_mode((WIDTH, HEIGHT))
-pg.display.set_caption("Manny's Game...")
+pg.display.set_caption("Manny's Pong Game...")
 clock = pg.time.Clock()
 
-def draw_text(text, size, color, x, y):
-        font_name = pg.font.match_font('arial')
-        font = pg.font.Font(font_name, size)
-        text_surface = font.render(text, True, color)
-        text_rect = text_surface.get_rect()
-        text_rect.midtop = (x, y)
-        screen.blit(text_surface, text_rect)
-  
+# Game loop
+running = True
+while running:
+    # keep the loop running using clock
+    clock.tick(FPS)
 
+    for event in pg.event.get():
+        # check for closed window
+        if event.type == pg.QUIT:
+            running = False
+
+
+    ############ Draw ################
+    # draw the background screen
+    screen.fill(WHITE)
+   
