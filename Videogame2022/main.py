62c44d701787d86f8705e329c38a26292f143d5d
emanuelwet <Emanuel.wetschnig@gmail.com>
2022-12-12 14:29:28 -0800

    Finally everything is coming together

--- Videogame2022/main.py
+++ Videogame2022/main.py
@@ -1,26 +1,25 @@
-# EMANUEL WETSCHNIG
+#EMANUEL WETSCHNIG
 
+
+##### SOURCES #####
 # content from kids can code: http://kidscancode.org/blog/
 # help from geeks for geeks: https://www.geeksforgeeks.org/
+# help from https://www.youtube.com/watch?v=C6jJg9Zan7w&ab_channel=freeCodeCamp.org
 
 
-import os
 # turtle will allow me to create graphic illustrations
-import turtle
-
-
-import os
 import turtle
 
 #create background
 screen = turtle.Screen()
 screen.title("Manny's Pong Game...")
-screen.bgcolor("White")
-screen.setup(width=1050, height=650)
+screen.bgcolor("Purple")
+screen.setup(width=800, height=650)
+screen.tracer(0) #stops the window from updating, calls for manual update (allows to speedup games) )
 
 
 #create vertical moving platform
-#platform 
+#platform  
 
 plat1 = turtle.Turtle() #automatically creates a background screen
 plat1.speed(5) #anywhere between 0-10
@@ -29,9 +28,18 @@ plat1.shape("square")
 #allows me to simply write the name of the color of the platform (I could also use the color code)
 plat1.color("black")
 #numbers have to be positive (width, length,)
-plat1.shapesize(stretch_wid=6, stretch_len=2)
+plat1.shapesize(stretch_wid=6, stretch_len=0.5)
 #"Pen-Up" simply means the created object does not draw anything on the sreen when moving
 plat1.penup()
 #allows me to set the position of the object 
-plat1.goto(10,10)
+plat1.goto(-350,0)
 
+
+
+
+
+
+
+
+while True:
+    screen.update()
