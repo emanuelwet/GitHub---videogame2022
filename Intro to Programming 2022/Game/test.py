13deca17ec99e5947449d79f7601c0df8e44e041
emanuelwet <Emanuel.wetschnig@gmail.com>
2022-12-12 14:08:51 -0800

    UPDATE

--- Intro to Programming 2022/Game/test.py
+++ Intro to Programming 2022/Game/test.py
@@ -1,87 +1,26 @@
-
-import pygame as pg
-from pygame.sprite import Sprite
+import os
 import turtle
 
+#create background
+screen = turtle.Screen()
+screen.title("Manny's Pong Game...")
+screen.bgcolor("White")
+screen.setup(width=1050, height=650)
 
-# game settings 
-WIDTH = 450
-HEIGHT = 500
-FPS = 30
-
-# define colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-GRAY = (128, 128, 128, 255)
-
-
-# initiate pygame and window is created
-pg.init()
-pg.mixer.init()
-screen = pg.display.set_mode((WIDTH, HEIGHT))
-pg.display.set_caption("Manny's Game...")
-clock = pg.time.Clock()
-  
-
-def draw_text(text, size, color, x, y):
-        font_name = pg.font.match_font('arial')
-        font = pg.font.Font(font_name, size)
-        text_surface = font.render(text, True, color)
-        text_rect = text_surface.get_rect()
-        text_rect.midtop = (x, y)
-        screen.blit(text_surface, text_rect)
-
-# platforms
-class Platform(Sprite):
-    def __init__(self, x, y, w, h):
-        Sprite.__init__(self)
-        self.image = pg.Surface((w, h))
-        self.image.fill(BLACK)
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
 
 #create vertical moving platform
 #platform 
-plat = turtle.Turtle()
-plat.speed(5)
-plat.shape("square")
-plat.color("black")
-plat.shapesize(stretch_wid=6, stretch_len=2)
-plat.penup()
-plat.goto(100,50)
 
+plat1 = turtle.Turtle() #automatically creates a background screen
+plat1.speed(5) #anywhere between 0-10
+#using this command I can simply type in the shape I want (given that it exists in the Turtle Screen's shape dictionary)
+plat1.shape("square")
+#allows me to simply write the name of the color of the platform (I could also use the color code)
+plat1.color("black")
+#numbers have to be positive (width, length,)
+plat1.shapesize(stretch_wid=6, stretch_len=2)
+#"Pen-Up" simply means the created object does not draw anything on the sreen when moving
+plat1.penup()
+#allows me to set the position of the object 
+plat1.goto(10,10)
 
-
-#create the groups
-all_plats = pg.sprite.Group()
-
-
-# plateforms are created (size + position)
-player = Platform(10, 50, 50, 35)
-plat1 = Platform(20, 40, 50, 35)
-plat1.image.fill(BLACK)
-
-
-
-
-
-# The Game loop
-running = True
-while running:
-    clock.tick(FPS)
-    dt = clock.tick(FPS)
-
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
-
-
-# -----here are drawing instructions-----
-        # background is drawn
-        screen.fill(WHITE)
-        # buffer - after drawing everything, flip display
-        pg.display.flip()
--- Videogame2022/main.py
+++ Videogame2022/main.py
@@ -1,73 +1,37 @@
+# EMANUEL WETSCHNIG
+
 # content from kids can code: http://kidscancode.org/blog/
 # help from geeks for geeks: https://www.geeksforgeeks.org/
 
 
-import pygame as pg
+import os
 # turtle will allow me to create graphic illustrations
 import turtle
 
 
-# game settings 
-WIDTH = 350
-HEIGHT = 300
-FPS = 30
+import os
+import turtle
 
-# define colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-GRAY = (128, 128, 128, 255)
-
-
-# initiate pygame and window is created
-pg.init()
-pg.mixer.init()
-screen = pg.display.set_mode((WIDTH, HEIGHT))
-pg.display.set_caption("Manny's Game...")
-clock = pg.time.Clock()
-  
-
-def draw_text(text, size, color, x, y):
-        font_name = pg.font.match_font('arial')
-        font = pg.font.Font(font_name, size)
-        text_surface = font.render(text, True, color)
-        text_rect = text_surface.get_rect()
-        text_rect.midtop = (x, y)
-        screen.blit(text_surface, text_rect)
+#create background
+screen = turtle.Screen()
+screen.title("Manny's Pong Game...")
+screen.bgcolor("White")
+screen.setup(width=1050, height=650)
 
 
 #create vertical moving platform
 #platform 
 
-plat = turtle.Turtle() #automatically creates a background screen
-plat.speed(5)
+plat1 = turtle.Turtle() #automatically creates a background screen
+plat1.speed(5) #anywhere between 0-10
 #using this command I can simply type in the shape I want (given that it exists in the Turtle Screen's shape dictionary)
-plat.shape("square")
-#alows me to simply write the name of the color (I could also use the color code)
-plat.color("black")
+plat1.shape("square")
+#allows me to simply write the name of the color of the platform (I could also use the color code)
+plat1.color("black")
 #numbers have to be positive (width, length,)
-plat.shapesize(stretch_wid=6, stretch_len=2)
+plat1.shapesize(stretch_wid=6, stretch_len=2)
 #"Pen-Up" simply means the created object does not draw anything on the sreen when moving
-plat.penup()
+plat1.penup()
 #allows me to set the position of the object 
-plat.goto(10,10)
+plat1.goto(10,10)
 
-
-
-# The Game loop
-running = True
-while running:
-    clock.tick(FPS)
-    dt = clock.tick(FPS)
-
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
-
-
-# -----here are drawing instructions-----
-        # background is drawn
-        screen.fill(WHITE)
-        # buffer - after drawing everything, flip display
-        pg.display.flip()
