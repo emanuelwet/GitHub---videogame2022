796c86c6cd1014f2712e0de3402ee33ba203064e
emanuelwet <Emanuel.wetschnig@gmail.com>
2022-12-14 19:03:02 -0800

    UPDATE

--- Videogame2022/main.py
+++ Videogame2022/main.py
@@ -11,58 +11,57 @@
 import pygame
 from pygame.locals import *
 
+#background image from files
+
+bg = pygame.image.load("C:\GitHub\Intro to Programming 2022\Videogame2022\heavencity.png")
+
 pygame.init()
 
-screen_width = 600
-screen_height = 600
-
+#Screen settings
+screen_width = 800
+screen_height = 650
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Manny's Keep Up Game")
+pygame.display.set_caption("Manny's Keep-Up Game")
 
-#define font
+#text font (for all text)
 font = pygame.font.SysFont('Times New Roman', 30)
 
-#define colours
-bg = (234, 218, 184)
-#block colours
-block_red = (242, 85, 96)
-block_green = (86, 174, 87)
-block_blue = (69, 177, 232)
-#plat1 colours
-plat1_col = (142, 135, 123)
-plat1_outline = (100, 100, 100)
-#text colour
-text_col = (78, 81, 139)
+#define colors
+gameball = (234, 218, 184)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+GRAY = (128, 128, 128, 255)
 
+#platform and gameball colors
+plat1_col = (WHITE)
+plat1_outline = (GRAY)
 
+#text color
+text_col = (RED)
 
-#define game variables
+#define game settings
 cols = 6
 rows = 6
 clock = pygame.time.Clock()
 fps = 60
-live_ball = False
+live_gb = False
 game_over = 0
 
-
-#function for outputting text onto the screen
+#getting text on the screen
 def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     screen.blit(img, (x, y))
 
-
-
-
-
-
-#plat1 class
+#platform class
 class plat1():
     def __init__(self):
         self.reset()
 
-
     def move(self):
-        #reset movement direction
+        #moevement direction gets reset
         self.direction = 0
         key = pygame.key.get_pressed()
         if key[pygame.K_a] and self.rect.left > 0:
@@ -78,7 +77,7 @@ class plat1():
 
 
     def reset(self):
-        #define plat1 variables
+        #define platform variables
         self.height = 20
         self.width = int(screen_width / cols)
         self.x = int((screen_width / 2) - (self.width / 2))
@@ -88,21 +87,17 @@ class plat1():
         self.direction = 0
 
 
-#ball class
-class game_ball():
+#game ball class
+class game_gb():
     def __init__(self, x, y):
         self.reset(x, y)
 
-
     def move(self):
-
         #collision threshold
         collision_thresh = 5
-
-        #check for collision with walls
+        #wall collision
         if self.rect.left < 0 or self.rect.right > screen_width:
             self.speed_x *= -1
-
         #check for collision with top and bottom of the screen
         if self.rect.top < 0:
             self.speed_y *= -1
@@ -110,9 +105,9 @@ class game_ball():
             self.game_over = -1
 
 
-        #look for collission with plat1
+        #collission with the platform
         if self.rect.colliderect(player_plat1):
-            #check if colliding from the top
+            #the game ball is making contact from the top
             if abs(self.rect.bottom - player_plat1.rect.top) < collision_thresh and self.speed_y > 0:
                 self.speed_y *= -1
                 self.speed_x += player_plat1.direction
@@ -123,8 +118,6 @@ class game_ball():
             else:
                 self.speed_x *= -1
 
-
-
         self.rect.x += self.speed_x
         self.rect.y += self.speed_y
 
@@ -132,16 +125,16 @@ class game_ball():
 
 
     def draw(self):
-        pygame.draw.circle(screen, plat1_col, (self.rect.x + self.ball_rad, self.rect.y + self.ball_rad), self.ball_rad)
-        pygame.draw.circle(screen, plat1_outline, (self.rect.x + self.ball_rad, self.rect.y + self.ball_rad), self.ball_rad, 3)
+        pygame.draw.circle(screen, plat1_col, (self.rect.x + self.gb_rad, self.rect.y + self.gb_rad), self.gb_rad)
+        pygame.draw.circle(screen, plat1_outline, (self.rect.x + self.gb_rad, self.rect.y + self.gb_rad), self.gb_rad, 3)
 
 
 
     def reset(self, x, y):
-        self.ball_rad = 10
-        self.x = x - self.ball_rad
+        self.gb_rad = 10
+        self.x = x - self.gb_rad
         self.y = y
-        self.rect = Rect(self.x, self.y, self.ball_rad * 2, self.ball_rad * 2)
+        self.rect = Rect(self.x, self.y, self.gb_rad * 2, self.gb_rad * 2)
         self.speed_x = 4
         self.speed_y = -4
         self.speed_max = 5
@@ -149,37 +142,36 @@ class game_ball():
 
 
 
-
-
-#create plat1
+#platform is created
 player_plat1 = plat1()
 
 
-#create ball
-ball = game_ball(player_plat1.x + (player_plat1.width // 2), player_plat1.y - player_plat1.height)
+#gameball is created
+gb = game_gb(player_plat1.x + (player_plat1.width // 2), player_plat1.y - player_plat1.height)
 
 run = True
 while run:
 
     clock.tick(fps)
-    
-    screen.fill(bg)
+    #INSIDE OF THE GAME LOOP
+    screen.blit(bg, (0, 0))
+    #screen.fill(GRAY)
 
     #draw all objects
     player_plat1.draw()
-    ball.draw()
+    gb.draw()
 
-    if live_ball:
+    if live_gb:
         #draw plat1
         player_plat1.move()
-        #draw ball
-        game_over = ball.move()
+        #draw gb
+        game_over = gb.move()
         if game_over != 0:
-            live_ball = False
+            live_gb = False
 
 
     #print player instructions
-    if not live_ball:
+    if not live_gb:
         if game_over == 0:
             draw_text('CLICK ANYWHERE TO START', font, text_col, 100, screen_height // 2 + 100)
         elif game_over == 1:
@@ -193,12 +185,10 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        if event.type == pygame.MOUSEBUTTONDOWN and live_ball == False:
-            live_ball = True
-            ball.reset(player_plat1.x + (player_plat1.width // 2), player_plat1.y - player_plat1.height)
+        if event.type == pygame.MOUSEBUTTONDOWN and live_gb == False:
+            live_gb = True
+            gb.reset(player_plat1.x + (player_plat1.width // 2), player_plat1.y - player_plat1.height)
             player_plat1.reset()
-
-
 
 
     pygame.display.update()
